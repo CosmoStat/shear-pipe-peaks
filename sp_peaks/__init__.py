9b16f78448cf1b29da5f0ffbf5514376020b9cf9
Martin Kilbinger <martin.kilbinger@cea.fr>
2023-07-27 10:43:59 +0200

    Installation via conda and pyproject; two py versions (3.11, 3.6); example slics script

---
+++ sp_peaks/__init__.py
@@ -0,0 +1,25 @@
+"""sp_peaks PACKAGE.
+
+CosmoStat utilities for weak lensing peaks from ShapePipe processed data.
+
+References
+----------
+This package makes use of the following third-party packages:
+
+- `Numpy <https://numpy.org/>`_ :cite:`Harris:2020`
+
+.. warning::
+
+    `WPS410 <https://wemake-python-stylegui.de/en/latest/pages/usage/violation
+    s/best_practices.html#wemake_python_styleguide.violations.best_practices.W
+    rongModuleMetadataViolation>`_ and `WPS412 <https://wemake-python-stylegui.
+    de/en/latest/pages/usage/violations/best_practices.html#wemake_python_style
+    guide.violations.best_practices.InitModuleHasLogicViolation>`_ errors are
+    supressed in this module to allow the defintion of a package
+    ``__version__``, which is standard for most Python packages.
+
+"""
+
+from warnings import warn
+
+__version__ = "0.0.1"
