0f316eac6e1bb45651674316e8ea37dfb8062880
Martin Kilbinger <martin.kilbinger@cea.fr>
2023-07-28 16:51:39 +0200

    starting n(z) resampling

--- example/test_slics.py
+++ example/test_slics.py
@@ -20,6 +20,10 @@
 # %load_ext autoreload                                                          
 # %autoreload 2                                                                                                                                                 
 
+import matplotlib.pylab as plt
+import os
+from cs_util import cat as cs_cat
+
 # +
 import sp_peaks           
 print(f"sp_peaks version = {sp_peaks.__version__}")
@@ -27,10 +31,15 @@ print(f"sp_peaks version = {sp_peaks.__version__}")
 from sp_peaks import slics
 # -
 
-cat_path = "/n17data/tersenov/SLICS/Cosmo_DES/06_f/LOS3/DES_MocksCat_06_f_4_Bin3_LOS3_R19.dat"
+root_directory = "/n17data/tersenov/SLICS/Cosmo_DES"
+
+cat_path = f"{root_directory}/06_f/LOS3/DES_MocksCat_06_f_4_Bin3_LOS3_R19.dat"
 
 # Load catalogue, all columns
 dat = slics.read_catalogue(cat_path)
+
+# DEBUG
+dat_comb = dat
 
 # Print column names
 print(dat.dtype.names)
@@ -41,6 +50,53 @@ print(dat[0])
 # Load only essential columns
 dat_ess = slics.read_catalogue(cat_path, all_col=False)
 
+from astropy.table import vstack
+x = vstack([dat, dat])
+
+len(dat)
+
 print(dat_ess[0])
+
+# Combine all four redshift bins for given cosmo ID, line of sight, and tile number
+dat_comb = slics.read_multiple_catalogues(
+    root_directory,
+    cosmo_id="fid_f",
+    zbins=None,
+    lsos=[2],
+    tiles=[5],
+    combine="add",
+    verbose=True,
+)
+
+len(dat_comb)
+
+# +
+# Read CFIS redshift distribution (blind version "A", ShapePipe)
+dndz_CFIS_path = "/n17data/mkilbing/astro/data/CFIS/v1.0/nz/dndz_SP_A.txt"
+z_centers_ext, dndz_ext, z_edges_ext = cs_cat.read_dndz(dndz_CFIS_path)
+
+nz = len(z_edges_ext)
+print(len(z_centers_ext), len(dndz_ext), len(z_edges_ext))
+# -
+
+dndz_slics, z_edges_slics = np.histogram(dat_comb["redshift_true_sim"], bins=z_edges_CFIS)
+
+# CHeck that z
+max(z_edges_slics - z_edges_ext)
+
+plt.plot(res[1][:-1], res[0] / sum(res[0]), '-')
+plt.plot(z_centers_ext, dndz_ext / sum(dndz_ext), '-')
+
+res[1][np.where(res[0] == 0)]
+
+z_max = 1.8
+
+idx_z_max = np.where(z_edges_ext < z_max)                               
+dndz_ext = dndz_ext[idx_z_max]                                          
+dndz_slics = dndz_slics[idx_z_max] 
+
+probability = dndz_ext / dndz_slics 
+
+probability
 
 
--- sp_peaks/slics.py
+++ sp_peaks/slics.py
@@ -8,6 +8,15 @@
 :Authors: Martin Kilbinger <martin.kilbinger@cea.fr>
 """
 
+import os
+import numpy as np
+
+from astropy.io import ascii
+from astropy.table import vstack
+
+from cs_util import cat as cs_cat
+
+
 col_names = [
     "RA",
     "Dec",
@@ -24,14 +33,14 @@ col_names = [
 col_names_essential = [
     "RA",
     "Dec",
-    "e1_data",
-    "e2_data",
+    "gamma1_sim",
+    "gamma2_sim",
     "redshift_true_sim",
 ]
 
-
-import os
-from astropy.io import ascii
+max_los = 4
+max_zbin = 4
+max_tile = 19
 
 
 def read_catalogue(file_path, all_col=True):
@@ -59,7 +68,7 @@ def read_catalogue(file_path, all_col=True):
 
     """
     if not os.path.exists(file_path):
-        raise IOError(f"SLICS catlogue file {file_path} does not exist")
+        raise IOError(f"SLICS catalogue file {file_path} does not exist")
 
     if all_col:
         include_names = None
@@ -69,3 +78,131 @@ def read_catalogue(file_path, all_col=True):
     dat = ascii.read(file_path, names=col_names, include_names=include_names)
 
     return dat
+
+
+def get_cat_name(cosmo_id, zbin, los, tile):
+    """Get Cat Name.
+
+    Return catalogue file name.
+
+    Parameters
+    ----------
+    cosmo_id : str
+        cosmology model ID
+    zbin : int
+        redshift bin number
+    los : int
+        line-of-sight number
+    tile : int
+        tile number (realisation)
+
+    Returns
+    -------
+    str
+        catalogue name
+
+    """
+    cat_name = (
+        f"{cosmo_id}/LOS{los}/DES_MocksCat_{cosmo_id}_4_Bin{zbin}_LOS{los}_"
+        + f"R{tile}.dat"
+    )
+
+    return cat_name
+
+
+def read_multiple_catalogues(
+    root_directory,
+    cosmo_id="fid_f",
+    zbins=None,
+    lsos=None,
+    tiles=None,
+    combine="add",
+    verbose=False,
+):
+    """Read Multiple Catalogues.
+
+    Read and combine a number of SLICS catalogues.
+
+    Parameters
+    ----------
+    root_directory : str
+        root directory of input catalogues
+    cosmo_id : str
+        cosmology model ID
+    zbins : list
+        list of redshift bins to combine, default is None (combine all)
+    lsos : list
+        list of lines of sight numbers to combine, default is None
+        (combine all)
+    tiles : list
+        list of tile numbers to combine, default is None (combine all)
+
+    Returns
+    -------
+    dict
+        catalogue content
+
+    """
+    if not os.path.exists(root_directory):
+        raise IOError(f"Root directory {root_directory} does not exist")
+
+    if not zbins:
+        zbins = np.arange(max_zbin) + 1
+    if not lsos:
+        lsos = np.arange(max_los) + 1
+    if not tiles:
+        tiles = np.arrange(max_tile) + 1
+
+    dat_comb = None
+
+    # Loop over input catalogue identifiers
+    for zbin in zbins:
+        for los in lsos:
+            for tile in tiles:
+
+                # Get single catalogue
+                cat_name = get_cat_name(cosmo_id, zbin, los, tile)
+                cat_path = f"{root_directory}/{cat_name}"
+
+                if verbose:
+                    print(f"Reading catalogue {cat_name}...")
+                dat = read_catalogue(cat_path, all_col=False)
+
+                if dat_comb is None:
+                    # First time: Copy catalogue
+                    dat_comb = dat.copy()
+                else:
+                    # Other times: Combine with previous
+                    if combine == "add":
+                        dat_comb = vstack([dat_comb, dat])
+                    else:
+                        pass
+
+    return dat_comb
+
+
+def resample_z(dat, dndz_path, z_max=None):
+
+    # Read external dndz file
+    z_centers_ext, dndz_ext, z_edges_ext = cs_cat.read_dndz(dndz_path)
+
+    # Create redshift histogram of SLICS catalogue, using external zbins
+    dndz_slics, z_edges_slics = np.histogram(
+        dat["redshift_true_sim"],
+        bins=z_edges_ext
+    )
+
+    # Cut redshifts if desired
+    if z_max is not None:
+        idx_z_max = np.where(z_edges_ext < z_max)
+        dndz_ext = dndz_ext[idx_z_max]
+        dndz_slics = dndz_slics[idx_z_max]
+
+    # Normalise redshift distributions
+    dndz_ext = dndz_ext / sum(dndz_ext)
+    dndz_slics = dndz_slics / sum(dndz_slics)
+
+    probability = dndz_ext / dndz_slics
+
+    
+
