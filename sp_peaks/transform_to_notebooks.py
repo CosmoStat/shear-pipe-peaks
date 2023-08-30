57579161071570da3adb88fa37c4ec44712f575d
Martin Kilbinger <martin.kilbinger@cea.fr>
2023-08-30 16:18:53 +0200

    transformer script style

--- sp_peaks/transform_to_notebooks.py
+++ sp_peaks/transform_to_notebooks.py
@@ -4,11 +4,19 @@ import os
 import jupytext
 
 def transform_to_notebooks():
+    """Transform to Notebooks.
+
+    Transform python scripts to jupyter notbooks.
+
+    """
     notebook_dir = "./notebooks"
 
     for filename in os.listdir(notebook_dir):
         if filename.endswith(".py"):
-            nb_file = os.path.join(notebook_dir, f"{os.path.splitext(filename)[0]}.ipynb")
+            nb_file = os.path.join(
+                notebook_dir,
+                f"{os.path.splitext(filename)[0]}.ipynb",
+            )
             if not os.path.exists(nb_file):
                 py_file = os.path.join(notebook_dir, filename)
                 jupytext.write(jupytext.read(py_file), nb_file)
@@ -16,9 +24,12 @@ def transform_to_notebooks():
             else:
                 print(f"{nb_file} exists")
 
+
 def main():
-    print("MKDEBUG")
     transform_to_notebooks()
+
+    return 0
+
 
 if __name__ == "__main__":
     main()
