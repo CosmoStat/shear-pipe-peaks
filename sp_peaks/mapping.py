2ae25be843ae4dea8a9b8dfb1fd9520d36680652
lbaumo <lucie.baumont@gmail.com>
2023-08-31 12:46:43 +0200

    created new modules

---
+++ sp_peaks/mapping.py
@@ -0,0 +1,8 @@
+"""MAPPING.
+
+:Name: mapping.py
+ 
+:Description: This package contains methods to create kappa maps and snr maps from shear catalogs                      
+                                                                               
+:Authors: Lucie Baumont <lucie.baumont@cea.fr> Martin Kilbinger <martin.kilbinger@cea.fr> 
+"""
