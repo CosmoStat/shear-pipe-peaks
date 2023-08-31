7a4092a0a81ca41bf6a908311cc958ccc0e77da6
lbaumo <lucie.baumont@gmail.com>
2023-08-31 12:46:43 +0200

    created new modules

---
+++ sp_peaks/summary_statistics.py
@@ -0,0 +1,8 @@
+"""SUMMARY STATISTICS.
+
+:Name: summary_statistics.py
+ 
+:Description: This package contains methods to compute summary statistics from mass maps                      
+                                                                               
+:Authors: Lucie Baumont <lucie.baumont@cea.fr> Martin Kilbinger <martin.kilbinger@cea.fr> 
+"""
