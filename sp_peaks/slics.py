68b1f7cbcad6f929180b92dd209be94a25ee8c5e
Andreas Tersenov <[andrewtersenov@gmail.com>
2023-09-26 20:52:09 +0200

    Moved the covariance bookkeeping functions into the slics module

--- sp_peaks/slics.py
+++ sp_peaks/slics.py
@@ -10,6 +10,7 @@
 import os
 from astropy.io import ascii
 import numpy as np
+import random
 
 
 
@@ -177,3 +178,70 @@ def map_cosmo_params_to_data(data, dat_file_path):
             print(f"No parameters found for ID {id}")
 
     return mapped_params
+
+
+def parse_cov_SLICS_filenames(file_paths):
+    # Make empty recarray to store the data
+    data = np.recarray(len(file_paths), dtype=[('bin', int), ('LOS', int), ('tile', int)])
+
+    # Iterate over the file paths and process each file
+    for i, file_path in enumerate(file_paths):
+        # Extract the file name from the file path
+        file_name = file_path.split("/")[-1]
+        
+        # Split file name into parts
+        file_parts = file_name.split("_")
+        
+        # Extract relevant information
+        bin = int(file_parts[4][3:])  # Extract the number after "Bin"
+        LOS = int(file_parts[5][3:])  # Extract the number after "LOS"
+        tile = int(file_parts[6][1:-4])  # Extract the number after "R"
+
+        # Assign the extracted data to the corresponding fields in the recarray
+        data[i]['bin'] = bin
+        data[i]['LOS'] = LOS
+        data[i]['tile'] = tile
+
+    return data
+
+def survey_realizations_reconstruction(num_realizations, num_tiles_per_realization, bin_number, los_numbers, file_paths):
+
+    # Create an empty list to store the collections of selected files for this bin
+    collections_of_files = []
+
+    # Create a set to keep track of selected filenames
+    selected_filenames = set()
+
+    # Iterate through realizations
+    for realization in range(num_realizations):
+        # Create an empty list to store the selected files for this realization
+        selected_tiles = []
+
+        # Create a list of available LOS numbers for this realization
+        available_los_numbers = list(los_numbers)
+
+        # Iterate through tiles
+        for tile_number in range(1, num_tiles_per_realization + 1):
+            # Initialize selected_file as None
+            selected_file = None
+
+            # Continue trying different LOS options until a matching file is found
+            while not selected_file:
+                # Randomly select a LOS from the available options
+                selected_los = random.choice(available_los_numbers)
+
+                # Generate the filename pattern for the selected LOS, bin, and tile
+                filename_pattern = f"Bin{bin_number}_LOS{selected_los}_R{tile_number}."
+
+                # Find the matching file in the list of file paths that has not been selected before
+                matching_files = [filename for filename in file_paths if filename_pattern in filename and filename not in selected_filenames]
+
+                if matching_files:
+                    selected_file = matching_files[0]
+                    selected_tiles.append(selected_file)
+                    selected_filenames.add(selected_file)
+
+        # Append the list of selected files for this realization to the collections_of_files
+        collections_of_files.append(selected_tiles)
+
+    return collections_of_files
